baf397a382efe04a6f9edc3a6e5b12b4be3039f0
boomer <frank.earnhardt@outlook.com>
2022-06-08 17:57:16 -0400

    2022_06_08-G

--- container/run.cmd
+++ container/run.cmd
@@ -2,4 +2,4 @@
 docker build -t vir_report .
 docker run -v %~dp0src_data:/opt/src_data --rm vir_report -mcron
 
-ECHO Local: %~dp0src_data\virs_report.csv
+ECHO Local: %~dp0src_data\virs_report.tsv
--- container/run.sh
+++ container/run.sh
@@ -2,3 +2,5 @@
 CWD=$(pwd)
 docker build -t vir_report .
 docker run -v $CWD/src_data:/opt/src_data --rm vir_report -mcron 
+
+ECHO LOCAL: $CWD/src_data/virs_report.tsv
