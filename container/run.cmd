3c8bf5fb09190bec4553a8d42d0d6f4d89368d6e
boomer <frank.earnhardt@outlook.com>
2022-06-08 17:55:44 -0400

    2022_06_08-G

--- container/run.cmd
+++ container/run.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 docker build -t vir_report .
-docker run -v %~dp0\src_data:/opt/src_data --rm vir_report -mcron
+docker run -v %~dp0src_data:/opt/src_data --rm vir_report -mcron
 
-ECHO Local: %~dp0\src_data\virs_report.csv
+ECHO Local: %~dp0src_data\virs_report.csv
